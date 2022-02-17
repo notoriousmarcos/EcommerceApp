0e8fccaa54e80d555b59d591276895c6855035d8
Marcos Brito <marcosvcbrito@gmail.com>
2022-02-17 16:49:41 -0300

    creation cart tests

---
+++ WhiteLabelECommerce/SharedTests/Domain/Model/CartTests.swift
@@ -0,0 +1,43 @@
+//
+//  CartTests.swift
+//  WhiteLabelECommerce
+//
+//  Created by Marcos Vinicius Brito on 17/02/22.
+//
+
+import XCTest
+@testable import WhiteLabelECommerce
+
+struct Cart {
+    let id: Int
+    let userId: Int
+    let date: Date
+    let products: [Product]
+}
+
+class CartTests: XCTestCase {
+    func testCart_init_ShouldRetainProperties() {
+        // Arrange
+        let creationDate = Date()
+        let product = Product(
+            id: 1,
+            title: "Product",
+            price: 10.0,
+            category: "men's clothing",
+            description: "Product description",
+            imageURL: "https:imageurl"
+        )
+        let sut = Cart(
+            id: 1,
+            userId: 1,
+            date: creationDate,
+            products: [product]
+        )
+
+       // Assert
+        XCTAssertEqual(sut.id, 1)
+        XCTAssertEqual(sut.userId, 1)
+        XCTAssertEqual(sut.date, creationDate)
+        XCTAssertEqual(sut.products, [product])
+    }
+}
