28fff382e106cd8fbf6676fdad41fbb503c9430b
Marcos Brito <marcosvcbrito@gmail.com>
2022-02-18 11:30:30 -0300

    RequestCreated

---
+++ WhiteLabelECommerce/Shared/Data/Network/Request/Request.swift
@@ -0,0 +1,55 @@
+//
+//  Request.swift
+//  TVMazeApp
+//
+//  Created by marcos.brito on 03/09/21.
+//
+
+import Foundation
+
+protocol Request {
+    var baseURL: String { get }
+    var method: HTTPMethod { get }
+    var contentType: String { get }
+    var params: [String: Any]? { get }
+    var body: [String: Any]? { get }
+    var headers: [String: String]? { get }
+    associatedtype ReturnType: Codable
+}
+
+extension Request {
+    private func requestBodyFrom(params: [String: Any]?) -> Data? {
+        guard let params = params else { return nil }
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: params, options: []) else {
+            return nil
+        }
+        return httpBody
+    }
+
+    private func makeURLComponents(_ baseURL: String, params: [String: Any]?) -> URL? {
+        guard let params = params else {
+            return URLComponents(string: baseURL)?.url
+        }
+
+        var urlComponents = URLComponents(string: baseURL)
+        urlComponents?.queryItems = params.map({ param in
+            URLQueryItem(name: param.key, value: param.value as? String)
+        })
+        return urlComponents?.url
+    }
+
+    func asURLRequest() -> URLRequest? {
+        guard let finalURL = makeURLComponents(baseURL, params: params) else {
+            return nil
+        }
+        var request = URLRequest(url: finalURL)
+        request.httpMethod = method.rawValue
+        request.httpBody = requestBodyFrom(params: body)
+        let defaultHeaders: [String: String] = [
+            "Content-Type": contentType,
+            "Accept": contentType
+        ]
+        request.allHTTPHeaderFields = defaultHeaders.merging(headers ?? [:], uniquingKeysWith: { first, _ in first })
+        return request
+    }
+}
---
+++ WhiteLabelECommerce/SharedTests/Data/Network/Request/RequestTests.swift
@@ -0,0 +1,56 @@
+//
+//  RequestTests.swift
+//  TVMazeAppTests
+//
+//  Created by marcos.brito on 04/09/21.
+//
+
+@testable import WhiteLabelECommerce
+import XCTest
+
+class RequestTests: XCTestCase {
+    func testRequest_asURLRequestWithAllProperties_ShouldReturnAValidURLRequest() {
+        // Arrange
+        let sut = MockRequest(params: ["AnyParam": "param"], body: ["AnyBody": "body"],
+                              headers: ["AnyHeader": "header"])
+
+        // Act
+        let urlRequest = sut.asURLRequest()
+
+        // Assert
+        XCTAssertNotNil(urlRequest)
+        XCTAssertEqual(urlRequest?.url?.absoluteString, "http://google.com?AnyParam=param")
+        XCTAssertNotNil(urlRequest?.httpBody)
+        XCTAssertNotNil(urlRequest?.allHTTPHeaderFields)
+        XCTAssertEqual(urlRequest?.allHTTPHeaderFields?["Content-Type"], "application/json")
+        XCTAssertEqual(urlRequest?.allHTTPHeaderFields?["Accept"], "application/json")
+        XCTAssertEqual(urlRequest?.allHTTPHeaderFields?["AnyHeader"], "header")
+    }
+
+    func testRequest_asURLRequestWithoutHeaderAndBody_ShouldReturnAValidURLRequest() {
+        // Arrange
+        let sut = MockRequest(params: nil, body: nil, headers: nil)
+
+        // Act
+        let urlRequest = sut.asURLRequest()
+
+        // Assert
+        XCTAssertNotNil(urlRequest)
+        XCTAssertEqual(urlRequest?.url?.absoluteString, "http://google.com")
+        XCTAssertNil(urlRequest?.httpBody)
+        XCTAssertNotNil(urlRequest?.allHTTPHeaderFields)
+        XCTAssertEqual(urlRequest?.allHTTPHeaderFields?["Content-Type"], "application/json")
+        XCTAssertEqual(urlRequest?.allHTTPHeaderFields?["Accept"], "application/json")
+    }
+}
+
+private struct MockRequest: Request {
+    typealias ReturnType = String
+
+    let baseURL: String = "http://google.com"
+    let method: HTTPMethod = .get
+    let contentType: String = "application/json"
+    let params: [String: Any]?
+    let body: [String: Any]?
+    let headers: [String: String]?
+}
