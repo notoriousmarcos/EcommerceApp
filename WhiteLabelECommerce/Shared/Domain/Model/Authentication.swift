f944017030285547078905422fca0bdbf3ae851e
Marcos Brito <marcosvcbrito@gmail.com>
2022-02-18 09:34:46 -0300

    Creating Authentication file

---
+++ WhiteLabelECommerce/Shared/Domain/Model/Authentication.swift
@@ -0,0 +1,12 @@
+//
+//  Authentication.swift
+//  WhiteLabelECommerce
+//
+//  Created by Marcos Vinicius Brito on 18/02/22.
+//
+
+import Foundation
+
+struct Authentication {
+    let token: String
+}
