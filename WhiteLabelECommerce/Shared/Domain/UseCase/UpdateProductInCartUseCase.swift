05175083602982f805f8325c0c5d08def1529497
Marcos Brito <marcosvcbrito@gmail.com>
2022-02-18 09:14:36 -0300

    UpdateProductInCartUseCase

---
+++ WhiteLabelECommerce/Shared/Domain/UseCase/UpdateProductInCartUseCase.swift
@@ -0,0 +1,17 @@
+//
+//  UpdateProductInCartUseCase.swift
+//  WhiteLabelECommerce
+//
+//  Created by Marcos Vinicius Brito on 18/02/22.
+//
+
+import Foundation
+
+protocol UpdateProductInCartUseCase {
+    typealias CompletionHandler = ResultCompletionHandler<Cart>
+    func execute(
+        _ product: Product,
+        inCart cart: Cart,
+        completion: @escaping CompletionHandler
+    )
+}
