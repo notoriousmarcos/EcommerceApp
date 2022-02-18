a2676e8e41f18779e4f06db459d70fda3a4fb6db
Marcos Brito <marcosvcbrito@gmail.com>
2022-02-18 08:59:04 -0300

    AddProductToCartUseCase

---
+++ WhiteLabelECommerce/Shared/Domain/UseCase/AddProductToCartUseCase.swift
@@ -0,0 +1,17 @@
+//
+//  AddProductToCartUseCase.swift
+//  WhiteLabelECommerce
+//
+//  Created by Marcos Vinicius Brito on 18/02/22.
+//
+
+import Foundation
+
+protocol AddProductToCartUseCase {
+    typealias CompletionHandler = ResultCompletionHandler<Cart>
+    func execute(
+        _ product: Product,
+        toCart cart: Cart,
+        completion: @escaping CompletionHandler
+    )
+}
