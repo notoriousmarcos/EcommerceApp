687081e0c9f2eef99c0e270e3401ea144fe52d8f
Marcos Brito <marcosvcbrito@gmail.com>
2022-02-18 09:28:16 -0300

    AuthenticationUseCase

---
+++ WhiteLabelECommerce/Shared/Domain/UseCase/AuthenticationUseCase.swift
@@ -0,0 +1,21 @@
+//
+//  AuthenticationUseCase.swift
+//  WhiteLabelECommerce
+//
+//  Created by Marcos Vinicius Brito on 18/02/22.
+//
+
+import Foundation
+
+protocol AuthenticationUseCase {
+    typealias CompletionHandler = ResultCompletionHandler<User>
+    func execute(
+        authenticationModel: AuthenticationModel,
+        completion: @escaping CompletionHandler
+    )
+}
+
+struct AuthenticationModel: Model {
+    var email: String
+    var password: String
+}
