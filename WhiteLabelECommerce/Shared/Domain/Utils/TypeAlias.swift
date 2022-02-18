aed4389d4772c8b414c8579325499f258e858e3e
Marcos Brito <marcosvcbrito@gmail.com>
2022-02-18 08:36:16 -0300

    GetAllProductsUseCase

---
+++ WhiteLabelECommerce/Shared/Domain/Utils/TypeAlias.swift
@@ -0,0 +1,10 @@
+//
+//  TypeAlias.swift
+//  WhiteLabelECommerce
+//
+//  Created by Marcos Vinicius Brito on 18/02/22.
+//
+
+import Foundation
+
+typealias ResultCompletionHandler<T> = (Result<T, Error>) -> Void
