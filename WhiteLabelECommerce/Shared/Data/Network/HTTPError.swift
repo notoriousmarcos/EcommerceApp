1853ad709764f1e4ada81c67fdb6f77ade5fdf58
Marcos Brito <marcosvcbrito@gmail.com>
2022-02-18 10:57:27 -0300

    Adding Network to Data layer

---
+++ WhiteLabelECommerce/Shared/Data/Network/HTTPError.swift
@@ -0,0 +1,37 @@
+//
+//  HTTPError.swift
+//  Shortly
+//
+//  Created by marcos.brito on 28/08/21.
+//
+
+import Foundation
+
+enum HTTPError: Int, Error {
+    case badRequest = 400
+    case unauthorized = 401
+    case forbidden = 403
+    case notFound = 404
+    case timeOut = 408
+    case serverError = 500
+    case unknown = -1
+
+    public init(rawValue: Int) {
+        switch rawValue {
+            case 400:
+                self = .badRequest
+            case 401:
+                self = .unauthorized
+            case 403:
+                self = .forbidden
+            case 404:
+                self = .notFound
+            case 408:
+                self = .timeOut
+            case 500:
+                self = .serverError
+            default:
+                self = .unknown
+        }
+    }
+}
---
+++ WhiteLabelECommerce/Shared/Data/Network/HTTPMethod.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPMethod.swift
+//  TVMazeApp
+//
+//  Created by marcos.brito on 04/09/21.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get     = "GET"
+    case post    = "POST"
+    case put     = "PUT"
+    case delete  = "DELETE"
+}
