b0730a298cf58d7f49cdfe7997d8dba73dafd02f
Marcos Brito <marcosvcbrito@gmail.com>
2022-02-23 14:05:10 -0300

    Disable hardened runtime

---
+++ WhiteLabelECommerce/Pods/Target Support Files/Pods-WhiteLabelECommerce (iOS)-WhiteLabelECommerceTests(iOS)/Pods-WhiteLabelECommerce (iOS)-WhiteLabelECommerceTests(iOS)-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WhiteLabelECommerce__iOS__WhiteLabelECommerceTests_iOS_ : NSObject
+@end
+@implementation PodsDummy_Pods_WhiteLabelECommerce__iOS__WhiteLabelECommerceTests_iOS_
+@end
