42d94343720657756a6b873d0e45db38137ab9c2
Marcos Brito <marcosvcbrito@gmail.com>
2022-06-02 11:12:15 -0300

    Creating some integration tests and configuring schemes to not run tests randomly

---
+++ WhiteLabelECommerce/SharedTests/Integration/RemoteGetCurrentCartIntegrationTests.swift
@@ -0,0 +1,28 @@
+//
+//  RemoteGetCurrentCartIntegrationTests.swift
+//  WhiteLabelECommerce
+//
+//  Created by Marcos Vinicius Brito on 02/06/22.
+//
+
+@testable import WhiteLabelECommerce
+import XCTest
+
+class RemoteGetCurrentCartIntegrationTests: XCTestCase {
+    func testRemoteGetCurrentCartUseCase_integration_shouldGetSuccess() {
+        // Arrange
+        let sut = RemoteGetCurrentCartUseCase(client: RemoteGetCurrentCartClient(client: NativeHTTPClient()))
+        let expectation = expectation(description: "Wait response to get cart.")
+
+        // Act
+        sut.execute(userId: 1) { result in
+            // Assert
+            if case let .success(values) = result {
+                XCTAssertEqual(values.userId, 1)
+            }
+            expectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 5)
+    }
+}
