b521510df7f585fb3bd13bf75141bebc4b4cf9cb
Marcos Brito <marcosvcbrito@gmail.com>
2023-07-10 17:32:58 -0300

    Improving solution for network

--- WhiteLabelECommerce/Shared/Infra/Network/NativeHTTPClient.swift
+++ WhiteLabelECommerce/Shared/Infra/Network/NativeHTTPClient.swift
@@ -9,71 +9,63 @@ import Combine
 import Foundation
 
 class NativeHTTPClient: HTTPClient {
-    // MARK: - Properties
-    let session: URLSession
-    private var subscriptions = Set<AnyCancellable>()
+  // MARK: - Properties
+  let session: URLSession
+  private var subscriptions = Set<AnyCancellable>()
 
-    // MARK: - Init
-    init(session: URLSession = .shared) {
-        self.session = session
+  // MARK: - Init
+  init(session: URLSession = .shared) {
+    self.session = session
+  }
+
+  // MARK: - Functions
+  func dispatch<ReturnType: Codable>(
+    request: Request,
+    _ completion: @escaping ResultCompletionHandler<ReturnType, DomainError>
+  ) {
+    guard let request = request.asURLRequest() else {
+      completion(.failure(.requestError(error: .urlError)))
+      return
     }
 
-    // MARK: - Functions
-    func dispatch<ReturnType: Codable>(
-        request: Request,
-        _ completion: @escaping ResultCompletionHandler<ReturnType, DomainError>
-    ) {
-        guard let request = request.asURLRequest() else {
-            completion(.failure(.requestError(error: .urlError)))
-            return
-        }
-        var subscription: AnyCancellable?
-        var isComplete = false
-        subscription = dispatch(request: request).sink { [weak self] result in
-            switch result {
-                case .failure(let error):
-                    completion(.failure(.requestError(error: error)))
-                case .finished:
-                    break
-            }
-            isComplete = true
-            if let subscription = subscription {
-                self?.subscriptions.remove(subscription)
-            }
-        } receiveValue: { response in
-            do {
-                let parsedValue = try JSONDecoder().decode(ReturnType.self, from: response)
-                completion(.success(parsedValue))
-            } catch {
-                completion(.failure(.errorOnParsing(error: error)))
-            }
-        }
+    var subscription: AnyCancellable?
 
-        if let subscription = subscription, !isComplete {
-            subscriptions.insert(subscription)
+    session
+      .dataTaskPublisher(for: request)
+      .tryMap { data, response in
+        if let response = response as? HTTPURLResponse,
+           !(200...299).contains(response.statusCode) {
+          throw HTTPError(rawValue: response.statusCode)
         }
-    }
-
-    private func dispatch(request: URLRequest) -> AnyPublisher<Data, HTTPError> {
-        return session
-            .dataTaskPublisher(for: request)
-            .tryMap({ data, response in
-                if let response = response as? HTTPURLResponse,
-                   !(200...299).contains(response.statusCode) {
-                    throw HTTPError(rawValue: response.statusCode)
-                }
-                return data
-            })
-            .mapError { error in
-                guard let error = error as? HTTPError else {
-                    if let error = error as? URLError {
-                        return HTTPError(rawValue: error.errorCode)
-                    }
-                    return .unknown
-                }
-                return error
-            }
-            .receive(on: DispatchQueue.main)
-            .eraseToAnyPublisher()
-    }
+        return data
+      }
+      .mapError { error in
+        guard let error = error as? HTTPError else {
+          if let error = error as? URLError {
+            return HTTPError(rawValue: error.errorCode)
+          }
+          return .unknown
+        }
+        return error
+      }
+      .sink { [weak self] result in
+        switch result {
+          case .failure(let error):
+            completion(.failure(.requestError(error: error)))
+          case .finished:
+            break
+        }
+        if let subscription = subscription {
+          self?.subscriptions.remove(subscription)
+        }
+      } receiveValue: { response in
+        do {
+          let parsedValue = try JSONDecoder().decode(ReturnType.self, from: response)
+          completion(.success(parsedValue))
+        } catch {
+          completion(.failure(.errorOnParsing(error: error)))
+        }
+      }
+      .store(in: &subscriptions)
+  }
 }
